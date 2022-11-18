60771f737b10d49e2277cc81b0ce4b12d995802f
BrandonQ <brandonquintanillaq@gmail.com>
2022-11-17 19:48:28 -0500

    * ADD -> Comments in 1:n relation

--- app/src/main/java/com/plcoding/multipleroomtables/entities/relations/SchoolWithStudents.kt
+++ app/src/main/java/com/plcoding/multipleroomtables/entities/relations/SchoolWithStudents.kt
@@ -5,11 +5,12 @@ import androidx.room.Relation
 import com.plcoding.multipleroomtables.entities.School
 import com.plcoding.multipleroomtables.entities.Student
 
+//1:n relation. One School is parent of many students
 data class SchoolWithStudents(
     @Embedded val school: School,
     @Relation(
-        parentColumn = "schoolName",
-        entityColumn = "schoolName"
+        parentColumn = "schoolName",//PK of School
+        entityColumn = "schoolName"//FK of Student
     )
     val students: List<Student>
 )
